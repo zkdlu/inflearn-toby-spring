3ac7f2da9cecf41259da2cb0d7b5c080fca7c149
zkdlu <zkdlu159951@gmail.com>
2023-02-05 15:30:52 +0900

    spring context (GenericApplicationContext)

--- src/main/java/com/zkdlu/inflearn/MainApplication.java
+++ src/main/java/com/zkdlu/inflearn/MainApplication.java
@@ -2,7 +2,7 @@ package com.zkdlu.inflearn;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
-import org.springframework.http.HttpHeaders;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -15,10 +15,12 @@ import java.io.IOException;
 
 public class MainApplication {
     public static void main(String[] args) {
+        var applicationContext = new GenericApplicationContext();
+        applicationContext.registerBean(HelloApi.class);
+        applicationContext.refresh();
+
         var serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            HelloApi helloApi = new HelloApi();
-
             servletContext.addServlet("frontcontroller", new HttpServlet() {
                 @Override
                 protected void service(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
@@ -27,12 +29,11 @@ public class MainApplication {
                     if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
                         String name = req.getParameter("name");
 
+                        HelloApi helloApi = applicationContext.getBean(HelloApi.class);
                         String ret = helloApi.hello(name);
 
-                        resp.setStatus(HttpStatus.OK.value());
-                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                        resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
                         resp.getWriter().println(ret);
-
                     } else if (req.getRequestURI().equals("/users")) {
 
                     } else {
