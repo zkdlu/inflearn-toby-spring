4c2d7036700022e733db34466fbcd218fc5dd0b1
zkdlu <zkdlu159951@gmail.com>
2023-02-05 16:27:20 +0900

    TestRestTemplate

---
+++ src/test/java/com/zkdlu/inflearn/HelloApiTest.java
@@ -0,0 +1,24 @@
+package com.zkdlu.inflearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloApiTest {
+
+    @Test
+    void helloApi() {
+        TestRestTemplate rest = new TestRestTemplate();
+
+        ResponseEntity<String> res = rest.getForEntity("http://localhost:8080/hello?name={name}", String.class, "Spring");
+
+        assertThat(res.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(res.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).contains(MediaType.TEXT_PLAIN_VALUE);
+        assertThat(res.getBody()).isEqualTo("Hello Spring");
+    }
+}
