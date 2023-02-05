58064ffc5114d41106aa2654d0ab61b7e7c05900
zkdlu <zkdlu159951@gmail.com>
2023-02-05 16:14:45 +0900

    My SpringBootApplication

--- src/main/java/com/zkdlu/inflearn/MainApplication.java
+++ src/main/java/com/zkdlu/inflearn/MainApplication.java
@@ -1,12 +1,10 @@
 package com.zkdlu.inflearn;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @Configuration
@@ -23,24 +21,6 @@ public class MainApplication {
     }
 
     public static void main(String[] args) {
-        var applicationContext = new AnnotationConfigWebApplicationContext() {
-            @Override
-            protected void onRefresh() {
-                super.onRefresh();
-
-                ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
-                DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
-
-                WebServer webServer = serverFactory.getWebServer(servletContext -> {
-                    servletContext.addServlet("dispatcherServlet", dispatcherServlet)
-                            .addMapping("/*");
-                });
-
-                webServer.start();
-            }
-        };
-
-        applicationContext.register(MainApplication.class);
-        applicationContext.refresh();
+        MySpringApplication.run(MainApplication.class, args);
     }
 }
---
+++ src/main/java/com/zkdlu/inflearn/MySpringApplication.java
@@ -0,0 +1,31 @@
+package com.zkdlu.inflearn;
+
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class MySpringApplication {
+
+    public static void run(Class<?> applicationClass, String... args) {
+        var applicationContext = new AnnotationConfigWebApplicationContext() {
+            @Override
+            protected void onRefresh() {
+                super.onRefresh();
+
+                ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
+                DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
+
+                WebServer webServer = serverFactory.getWebServer(servletContext -> {
+                    servletContext.addServlet("dispatcherServlet", dispatcherServlet)
+                            .addMapping("/*");
+                });
+
+                webServer.start();
+            }
+        };
+
+        applicationContext.register(applicationClass);
+        applicationContext.refresh();
+    }
+}
