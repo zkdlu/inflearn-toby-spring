7ddcd2a0a817b56e6d06f9f52c3bf0c01381aa29
zkdlu <zkdlu159951@gmail.com>
2023-02-11 00:41:29 +0900

    ImportCandidates을 이용해 Config 설정파일 읽어오기

--- src/main/java/com/zkdlu/config/MyAutoConfigImportSelector.java
+++ src/main/java/com/zkdlu/config/MyAutoConfigImportSelector.java
@@ -1,14 +1,22 @@
 package com.zkdlu.config;
 
+import org.springframework.boot.context.annotation.ImportCandidates;
 import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
+import java.util.stream.StreamSupport;
+
 public class MyAutoConfigImportSelector implements DeferredImportSelector {
+
+    private final ClassLoader classLoader;
+
+    public MyAutoConfigImportSelector(final ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
+
     @Override
     public String[] selectImports(AnnotationMetadata importingClassMetadata) {
-        return new String[]{
-                "com.zkdlu.config.autoconfig.DispatcherServletConfig",
-                "com.zkdlu.config.autoconfig.TomcatWebServerConfig"
-        };
+        ImportCandidates candidates = ImportCandidates.load(MyAutoConfiguration.class, classLoader);
+        return StreamSupport.stream(candidates.spliterator(), false).toArray(String[]::new);
     }
 }
